c490857e7f8da6a6aff4a7e66fcf99d4cc2a0fc8
equitymarkets <derekprice@Dereks-MacBook-Air.local>
2023-04-23 00:20:23 -0400

    maps, CSS, flask routes

--- static/js/app_heat.js
+++ static/js/app_heat.js
@@ -1,132 +1,106 @@
-//Create map object ------old stuff-------placeholder!!!
-// let myMap = L.map("map", {
-//     center: [39.8283, -98.5795],
-//     zoom: 5
-// });
+let defaultURL4 = "/heatmap";
+let defaultURL5 = "/PopData"
+d3.json(defaultURL4).then(function(heat_data) {
+  d3.json(defaultURL5).then(function (choro_data){
+  console.log(heat_data)
+  console.log(choro_data)
 
-// //Create tile layer 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(myMap);
+let h_map_data = heat_data;
 
-//----------------------------------------------------------------------------------------------------------->
-//Create map object
-let myMap = L.map("map", {
-    center: [39.8283, -98.5795],
-    zoom: 5
-  });
-  
-  //Create tile layer 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+//Create tile layer 
+let baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 18
+});
 
-//Store our API route as queryUrl. 
-let defaultURL4 = "/geojson";
-d3.json(defaultURL4).then(function (data) {
-  // Console log the data retrieved 
-    // console.log(data.features);
-    // Once we get a response, send the data.features object to the createFeatures function.
-    createFeatures(data.features);
-    });
+//~~~~*~~~~ HEAT MAP CODE ~~~~*~~~~
 
-
-
-// Function to determine marker size
-function markerSize(magnitude) {
-  return magnitude * 3000;
+let points = {
+  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+  // if scaleRadius is false it will be the constant radius used in pixels
+  "radius": .8,
+  "maxOpacity": 1,
+  // scales the radius based on map zoom
+  "scaleRadius": true,
+  // if set to false the heatmap uses the global maximum for colorization
+  // if activated: uses the data maximum within the current map boundaries
+  //   (there will always be a red spot with useLocalExtremas true)
+  "useLocalExtrema": true,
+  // which field name in your data represents the latitude - default "lat"
+  latField: 'lat',
+  // which field name in your data represents the longitude - default "lng"
+  lngField: 'lon',
+  // which field name in your data represents the data value - default "value"
+  valueField: 'size'
 };
 
-// Function to determine marker color by depth
-function chooseColor(depth){
-  if (depth < 20) return "#00FF00";
-  else if (depth < 40) return "greenyellow";
-  else if (depth < 60) return "yellow";
-  else if (depth < 80) return "orange";
-  else if (depth < 100) return "orangered";
-  else return "#FF0000";
+let heatmapLayer =  new HeatmapOverlay(points);
+    heatmapLayer.setData(h_map_data);
+
+
+
+//~~~~*~~~~ POP DEN CHORO MAP CODE ~~~~*~~~~
+
+
+// get color depending on population density value
+function getColor(d) {
+  return d > 500 ? '#99000d' :
+    d > 300  ? '#cb181d' :
+    d > 200  ? '#ef3b2c' :
+    d > 100  ? '#fb6a4a' :
+    d > 50   ? '#fc9272' :
+    d > 20   ? '#fcbba1' :
+    d > 10   ? '#fee0d2' : '#fff5f0';
 }
 
-// Create a map that plots all the fires from your dataset based on their longitude and latitude
-function createFeatures(_fire_props) {
-
-// Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the fire.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>Place:${feature.properties.place}</h3><hr><p>Time:${new Date(feature.properties.time)}</p></h3><hr><p>Magnitude: ${feature.properties.mag}</p>`);
-  }
-  // Create a GeoJSON layer that contains the features array on the fire object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  var fires = L.geoJSON(_fire_props, {
-    onEachFeature: onEachFeature,
-
-    // Point to layer used to alter markers
-    pointToLayer: function (feature, latlng) {
-      
-      // Determine the style of markers based on properties
-    //   var markers = {
-    //     radius: markerSize(7*feature.properties),
-    //     fillColor: chooseColor(feature.geometry.coordinates[2]),
-    //     color: "black",
-    //     weight: 1,
-    //     opacity: 1,
-    //     stroke: true,
-    //     fillOpacity: 0.8
-    //   };
-    //   return L.circle(latlng, markers);
-    }
-  });
-  // Send our fires layer to the createMap function
-  createMap(fires);
-}
-
-function createMap(fires) {
-  
-  // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-
-  // Create our map, giving it the grayscale map and fires layers to display on load.
-  var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, fires]
-  });
-
-  // Add legend
-  var legend = L.control({position: 'bottomright'});
-
-  legend.onAdd = function () {
-  
-      var div = L.DomUtil.create('div', 'info legend'),
-      depth = [-10, 10, 30, 50, 70, 90];
-
-      div.innerHTML+='Depth<br><hr>'
-  
-      // Loop through density intervals 
-      for (var i = 0; i < depth.length; i++) {
-          div.innerHTML +=
-          '<i style="background:' + chooseColor(depth[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
-          depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
-  }
-  
-  return div;
+function style(feature) {
+  return {
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7,
+    fillColor: getColor(feature.properties.density)
   };
-  
-  legend.addTo(myMap);
-};
+}
+
+function highlightFeature(e) {
+  const layer = e.target;
+
+  layer.setStyle({
+    weight: 5,
+    color: '#666',
+    dashArray: '',
+    fillOpacity: 0.7
+  });
+
+}
+
+// Population density data //
+let geojson = L.geoJson(choro_data, {
+  style,
+  onEachFeature
+})
 
 
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+}
 
+function onEachFeature(_feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+  });
+}
+//~~~~*~~~~ CREATE MAP ~~~~*~~~~
 
+let mymap = L.map("map", {
+  center: [39.8283, -98.5795],
+  zoom: 4,
+  layers: [baseLayer, heatmapLayer, geojson]
+});
 
-
-
-
-
-
+  })
+});
 //----------------------------------------------------------------------------------------------------------->
